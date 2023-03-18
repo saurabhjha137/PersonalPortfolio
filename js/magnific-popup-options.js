1d456f8eb195102732a8779584bf7c6fd1304907
saurabhjha137 <saurabhjha137@gmail.com>
2023-03-18 13:53:58 +0530

    popup code added

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   // MagnificPopup
   var magnifPopup = function() {
@@ -29,4 +30,27 @@ $(document).ready(function() {
   // Call the functions 
   magnifPopup();
 
+});
+
+
+
+
+$(document).ready(function() {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
 });
